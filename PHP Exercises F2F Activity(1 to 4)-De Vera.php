b36f838a1ea0d7de27914e27dbb0518a1d80c466
Adrian Joed De Vera <113957281+B-Rabbit913@users.noreply.github.com>
2022-09-20 19:37:57 +0800

    Add files via upload

---
+++ PHP Exercises F2F Activity(1 to 4)-De Vera.php
@@ -0,0 +1,75 @@
+<HTML>
+<head>
+  <Title>PHP Exercises f2f Activity</title>
+</head>
+<body>
+<?php
+
+echo "exer 1";
+echo "<br>";
+echo  "Twinkle, twinkle little star\n";
+echo "<br>";
+$twe = "Twinkle";
+$str = "Star";
+echo "<br>";
+echo "$twe, $twe little $str\n";
+echo "<br>";
+$he = "Hello";
+$wd = "World";
+echo "<br>";
+echo "$he, $he little $wd\n";
+echo "<br>";
+echo "<br>";
+echo"exer 2";
+$x=10;
+$y=7;
+echo "<br>";
+echo "$x + $y =",$x+$y, "\n";
+echo "<br>";
+echo "$x - $y =",$x-$y, "\n";
+echo "<br>";
+echo "$x * $y =",$x*$y, "\n";
+echo "<br>";
+echo "$x / $y =",$x/$y, "\n";
+echo "<br>";
+echo "$x % $y =",$x%$y, "\n";
+echo "<br>";
+echo "<br>";
+echo "exer 3";
+echo "<br>";
+$variable = 8;
+echo "value is now $variable.\n";
+echo "<br>";
+echo "Add 2. Value is now ", $variable = $variable + 2,".\n";
+echo "<br>";
+echo "Subtract 4. Value is now ", $variable = $variable - 4,".\n";
+echo "<br>";
+echo "Multiply by 5. Value is now ", $variable = $variable * 5, ".\n";
+echo "<br>";
+echo "Divide by 3. Value is now ", $variable = $variable / 3, ".\n";
+echo "<br>";
+echo "Increment Value by one. Value is now ", $variable+1,".\n";
+echo "<br>";
+echo "Decrement Value by one. Value is now ", $variable--,".\n";
+echo "<br>";
+echo "<br>";
+echo "exer 4";
+echo "<br>";
+$name='Harry'; 
+$age=28;   
+ 
+var_dump($name); 
+echo "<br>";   
+ 
+print_r($name); 
+echo "<br>";   
+ 
+var_dump($age); 
+echo "<br>";  
+  
+$name=NULL;   
+var_dump($name);
+
+?>
+</body>
+</html>
